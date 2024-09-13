f73d715bb1b748a092d7271f322678fb1fbe7475
jacksonPrimo <jacksonp@speedio.com.br>
2024-09-13 15:22:38 -0300

    add create goal

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,35 @@
 import fastify from "fastify";
+import z from "zod";
+import { createGoal } from "../functions/create-foal";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+	"/goals",
+	{
+		schema: {
+			body: z.object({
+				title: z.string(),
+				desiredWeeklyFrequency: z.number().int().min(1).max(7),
+			}),
+		},
+	},
+	async (request) => {
+		const { title, desiredWeeklyFrequency } = request.body;
+		await createGoal({
+			title: title,
+			desiredWeeklyFrequency: desiredWeeklyFrequency,
+		});
+	},
+);
 
 app
 	.listen({
